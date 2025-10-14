479a2bbb3050eadb642fc608ae23036541a70052
dippal513 <dip.pal.513@gmail.com>
2025-10-14 19:31:04 +0600

    vercel file updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,10 @@ configDotenv()
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
